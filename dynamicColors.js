e6d9989dcad80b1b3c384026350ba9903c82e6f1
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-09-30 19:56:35 +0000

    Upload dynamicColors.js

---
+++ dynamicColors.js
@@ -0,0 +1,23 @@
+console.log("\nDynamic Colors\n");
+
+changeColor = (color) => {
+  return () => {
+    document.body.style.background = color
+  }
+}
+
+const bgColorBlue = changeColor("#0f62fe")
+const bgColorPink = changeColor("#ff7eb6")
+const bgColorGreen = changeColor("#42be65")
+
+document
+  .querySelector("#blue")
+  .addEventListener("click", bgColorBlue)
+
+document
+  .querySelector("#pink")
+  .addEventListener("click", bgColorPink)
+
+document
+  .querySelector("#green")
+  .addEventListener("click", bgColorGreen)
